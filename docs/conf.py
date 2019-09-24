13b55bb97c89e960d26b8fb8230a9fe2e7d76cbc
violafanfani <v.fanfani@sms.ed.ac.uk>
2019-09-24 13:39:30 +0100

    Bump version: 2.0.6-dev → 2.0.7-dev

--- docs/conf.py
+++ docs/conf.py
@@ -26,9 +26,9 @@ copyright = 'Viola Fanfani & Giovanni Stracquadanio'
 author = 'Viola Fanfani & Giovanni Stracquadanio'
 
 # The short X.Y version
-version = '2.0.6-dev'
+version = '2.0.7-dev'
 # The full version, including alpha/beta/rc tags
-release = '2.0.6-dev'
+release = '2.0.7-dev'
 
 
 # -- General configuration ---------------------------------------------------
