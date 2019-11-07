cfcc37aa52be4c042183b9de83196d239fe7ef1d
violafanfani <v.fanfani@sms.ed.ac.uk>
2019-11-07 10:47:55 +0000

    Bump version: 2.1.2-dev → 2.1.3-dev

--- docs/conf.py
+++ docs/conf.py
@@ -26,9 +26,9 @@ copyright = 'Viola Fanfani & Giovanni Stracquadanio'
 author = 'Viola Fanfani & Giovanni Stracquadanio'
 
 # The short X.Y version
-version = '2.1.2-dev'
+version = '2.1.3-dev'
 # The full version, including alpha/beta/rc tags
-release = '2.1.2-dev'
+release = '2.1.3-dev'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     # package information
     name = 'pygna',
     packages = find_packages(),
-    version = '2.1.2-dev',
+    version = '2.1.3-dev',
     description = 'Geneset Network Analysis',
     long_description = readme,
     license = 'MIT',
