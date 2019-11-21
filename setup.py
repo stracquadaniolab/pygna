7ffcb407b4f354096be96262d01c726e5a2292e7
violafanfani <v.fanfani@sms.ed.ac.uk>
2019-11-21 13:41:46 +0000

    Bump version: 2.2.1-dev → 2.2.2-dev

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     # package information
     name = 'pygna',
     packages = find_packages(),
-    version = '2.2.1-dev',
+    version = '2.2.2-dev',
     description = 'Geneset Network Analysis',
     long_description = readme,
     license = 'MIT',
