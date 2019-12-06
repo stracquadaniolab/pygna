beab9f6d1904fade3f45965dcd73c2784079407f
Fabio <fabio.cassano@ed.ac.uk>
2019-12-06 18:15:45 +0000

    Fixed compilation

--- pygna/reading_class.py
+++ pygna/reading_class.py
@@ -5,9 +5,6 @@ import tables
 import networkx as nx
 import logging
 import sys
-import numpy as np
-import scipy.sparse as sp
-import torch
 
 
 class ReadingData(object):
@@ -246,74 +243,74 @@ class ReadDistanceMatrix(ReadingData):
         return self.nodes, self.data
 
 
-class ReadNnDataset(ReadingData):
-    def __init__(self, filename, dataset_name):
-        super(ReadNnDataset, self).__init__()
-        self.filename = filename
-        self.dataset_name = dataset_name
-
-        self.adj, self.features, self.labels, self.idx_train, self.idx_val, self.idx_test = self.__readfile()
-
-    def __readfile(self):
-        """Load citation network dataset (cora only for now)"""
-        logging.info('Loading {} dataset...'.format(self.dataset_name))
-
-        idx_features_labels = np.genfromtxt("{}{}.content".format(self.filename, self.dataset_name),
-                                            dtype=np.dtype(str))
-        features = sp.csr_matrix(idx_features_labels[:, 1:-1], dtype=np.float32)
-        labels = self._encode_onehot(idx_features_labels[:, -1])
-
-        # build graph
-        idx = np.array(idx_features_labels[:, 0], dtype=np.int32)
-        idx_map = {j: i for i, j in enumerate(idx)}
-        edges_unordered = np.genfromtxt("{}{}.cites".format(self.filename, self.dataset_name), dtype=np.int32)
-        edges = np.array(list(map(idx_map.get, edges_unordered.flatten())), dtype=np.int32).reshape(
-            edges_unordered.shape)
-        adj = sp.coo_matrix((np.ones(edges.shape[0]), (edges[:, 0], edges[:, 1])),
-                            shape=(labels.shape[0], labels.shape[0]), dtype=np.float32)
-
-        # build symmetric adjacency matrix
-        adj = adj + adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
-
-        features = self._normalize_features(features)
-        adj = self._normalize_adj(adj + sp.eye(adj.shape[0]))
-
-        idx_train = range(140)
-        idx_val = range(200, 500)
-        idx_test = range(500, 1500)
-
-        adj = torch.FloatTensor(np.array(adj.todense()))
-        features = torch.FloatTensor(np.array(features.todense()))
-        labels = torch.LongTensor(np.where(labels)[1])
-
-        idx_train = torch.LongTensor(idx_train)
-        idx_val = torch.LongTensor(idx_val)
-        idx_test = torch.LongTensor(idx_test)
-
-        return adj, features, labels, idx_train, idx_val, idx_test
-
-    def _normalize_adj(self, mx):
-        """Row-normalize sparse matrix"""
-        rowsum = np.array(mx.sum(1))
-        r_inv_sqrt = np.power(rowsum, -0.5).flatten()
-        r_inv_sqrt[np.isinf(r_inv_sqrt)] = 0.
-        r_mat_inv_sqrt = sp.diags(r_inv_sqrt)
-        return mx.dot(r_mat_inv_sqrt).transpose().dot(r_mat_inv_sqrt)
-
-    def _normalize_features(self, mx):
-        """Row-normalize sparse matrix"""
-        rowsum = np.array(mx.sum(1))
-        r_inv = np.power(rowsum, -1).flatten()
-        r_inv[np.isinf(r_inv)] = 0.
-        r_mat_inv = sp.diags(r_inv)
-        mx = r_mat_inv.dot(mx)
-        return mx
-
-    def _encode_onehot(self, labels):
-        classes = set(labels)
-        classes_dict = {c: np.identity(len(classes))[i, :] for i, c in enumerate(classes)}
-        labels_one_hot = np.array(list(map(classes_dict.get, labels)), dtype=np.int32)
-        return labels_one_hot
-
-    def get_data(self):
-        return self.adj, self.features, self.labels, self.idx_train, self.idx_val, self.idx_test
+# class ReadNnDataset(ReadingData):
+#     def __init__(self, filename, dataset_name):
+#         super(ReadNnDataset, self).__init__()
+#         self.filename = filename
+#         self.dataset_name = dataset_name
+#
+#         self.adj, self.features, self.labels, self.idx_train, self.idx_val, self.idx_test = self.__readfile()
+#
+#     def __readfile(self):
+#         """Load citation network dataset (cora only for now)"""
+#         logging.info('Loading {} dataset...'.format(self.dataset_name))
+#
+#         idx_features_labels = np.genfromtxt("{}{}.content".format(self.filename, self.dataset_name),
+#                                             dtype=np.dtype(str))
+#         features = sp.csr_matrix(idx_features_labels[:, 1:-1], dtype=np.float32)
+#         labels = self._encode_onehot(idx_features_labels[:, -1])
+#
+#         # build graph
+#         idx = np.array(idx_features_labels[:, 0], dtype=np.int32)
+#         idx_map = {j: i for i, j in enumerate(idx)}
+#         edges_unordered = np.genfromtxt("{}{}.cites".format(self.filename, self.dataset_name), dtype=np.int32)
+#         edges = np.array(list(map(idx_map.get, edges_unordered.flatten())), dtype=np.int32).reshape(
+#             edges_unordered.shape)
+#         adj = sp.coo_matrix((np.ones(edges.shape[0]), (edges[:, 0], edges[:, 1])),
+#                             shape=(labels.shape[0], labels.shape[0]), dtype=np.float32)
+#
+#         # build symmetric adjacency matrix
+#         adj = adj + adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
+#
+#         features = self._normalize_features(features)
+#         adj = self._normalize_adj(adj + sp.eye(adj.shape[0]))
+#
+#         idx_train = range(140)
+#         idx_val = range(200, 500)
+#         idx_test = range(500, 1500)
+#
+#         adj = torch.FloatTensor(np.array(adj.todense()))
+#         features = torch.FloatTensor(np.array(features.todense()))
+#         labels = torch.LongTensor(np.where(labels)[1])
+#
+#         idx_train = torch.LongTensor(idx_train)
+#         idx_val = torch.LongTensor(idx_val)
+#         idx_test = torch.LongTensor(idx_test)
+#
+#         return adj, features, labels, idx_train, idx_val, idx_test
+#
+#     def _normalize_adj(self, mx):
+#         """Row-normalize sparse matrix"""
+#         rowsum = np.array(mx.sum(1))
+#         r_inv_sqrt = np.power(rowsum, -0.5).flatten()
+#         r_inv_sqrt[np.isinf(r_inv_sqrt)] = 0.
+#         r_mat_inv_sqrt = sp.diags(r_inv_sqrt)
+#         return mx.dot(r_mat_inv_sqrt).transpose().dot(r_mat_inv_sqrt)
+#
+#     def _normalize_features(self, mx):
+#         """Row-normalize sparse matrix"""
+#         rowsum = np.array(mx.sum(1))
+#         r_inv = np.power(rowsum, -1).flatten()
+#         r_inv[np.isinf(r_inv)] = 0.
+#         r_mat_inv = sp.diags(r_inv)
+#         mx = r_mat_inv.dot(mx)
+#         return mx
+#
+#     def _encode_onehot(self, labels):
+#         classes = set(labels)
+#         classes_dict = {c: np.identity(len(classes))[i, :] for i, c in enumerate(classes)}
+#         labels_one_hot = np.array(list(map(classes_dict.get, labels)), dtype=np.int32)
+#         return labels_one_hot
+#
+#     def get_data(self):
+#         return self.adj, self.features, self.labels, self.idx_train, self.idx_val, self.idx_test
