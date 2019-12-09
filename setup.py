ba8de0378d495f2165e6c2468c4a9ddf8113cd5e
violafanfani <v.fanfani@sms.ed.ac.uk>
2019-12-09 09:30:28 +0000

    conda CI major bumpversio

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     # package information
     name='pygna',
     packages=find_packages(),
-    version='2.3',
+    version='2.2.4-dev',
     description='Geneset Network Analysis',
     long_description=readme,
     license='MIT',
