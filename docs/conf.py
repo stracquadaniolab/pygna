60bf4afa883b0946dfdf035177064d2666a365c5
violafanfani <viola.fanfani@gmail.com>
2020-06-25 14:24:15 +0100

    Bump version: 3.1.7-dev → 3.1.8-dev

--- docs/conf.py
+++ docs/conf.py
@@ -28,9 +28,9 @@ copyright = 'Viola Fanfani & Giovanni Stracquadanio'
 author = 'Viola Fanfani & Giovanni Stracquadanio'
 
 # The short X.Y version
-version = '3.1.7-dev'
+version = '3.1.8-dev'
 # The full version, including alpha/beta/rc tags
-release = '3.1.7-dev'
+release = '3.1.8-dev'
 
 
 # -- General configuration ---------------------------------------------------
