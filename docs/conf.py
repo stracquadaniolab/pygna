d7eb4b582989d66f50184fd1c0d825102578e074
gstracqu <giovanni.stracquadanio@ed.ac.uk>
2020-06-28 11:28:35 +0100

    Bump version: 3.2.0 → 3.2.1

--- docs/conf.py
+++ docs/conf.py
@@ -28,9 +28,9 @@ copyright = 'Viola Fanfani & Giovanni Stracquadanio'
 author = 'Viola Fanfani & Giovanni Stracquadanio'
 
 # The short X.Y version
-version = '3.2.0'
+version = '3.2.1'
 # The full version, including alpha/beta/rc tags
-release = '3.2.0'
+release = '3.2.1'
 
 
 # -- General configuration ---------------------------------------------------
