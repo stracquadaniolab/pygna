4d29f65cb655ac6f32389cd0e3775006b6e8bd5d
violafanfani <viola.fanfani@gmail.com>
2020-09-22 15:02:13 +0100

    Bump version: 3.2.4 → 3.3.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     # package information
     name='pygna',
     packages=find_packages(),
-    version='3.2.4',
+    version='3.3.0',
     description='Geneset Network Analysis',
     long_description=readme,
     license='MIT',
